f8e032e538cf7dc2ab3afe4d96a67203bab1de00
J-Talon <94146034+J-Talon@users.noreply.github.com>
2025-11-08 19:58:07 -0500

    Revert "Merge Holdnotesfix into prototype"

--- Assets/Scripts/Notes/HoldNote.cs
+++ Assets/Scripts/Notes/HoldNote.cs
@@ -1,5 +1,3 @@
-using System.Drawing;
-using Unity.Properties;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,22 +8,7 @@ public class HoldNote : NoteBase
 {
     private bool isHolding = false; // there are ways to do this throught the input system that i am choosing not to do
     private bool hasStarted = false;
-
     private float holdTimer = 0f;
-    private float totalTime; // the amount of time of the note in seconds
-    private float noteLength; // the physical length of the note (dependant on speed)
-
-    private Transform pivotTransform; // reference to the pivot's transform for proper stretching
-
-    public void Start()
-    {
-        pivotTransform = transform.Find("Note Pivot");
-
-        totalTime = data.parameters["endTime"] - data.time;
-        noteLength = totalTime * speed;
-
-        pivotTransform.localScale += Vector3.right * noteLength - Vector3.right; // stretching the note to represent its length
-    }
 
     public override void OnKeyPressed()
     {
@@ -41,15 +24,11 @@ public class HoldNote : NoteBase
         base.Update();
         // *** DO NOT *** unprotect base.Update(). FOR ANY REASON! UNLESS CLEARED WITH ME!
 
-        if (hasStarted && isHolding)
+        if (isHolding)
         {
             if (InputManager.Instance.IsLaneHeld(lane.laneIndex))
             {
                 holdTimer += Time.deltaTime;
-
-                pivotTransform.localScale = new Vector3(noteLength - ((holdTimer) / totalTime) * speed, 1f, 1f); // shrinks it when it is being held
-                transform.position += Vector3.right * speed * Time.deltaTime; // keeps the front of the note in place
-
                 if (holdTimer >= (data.parameters["endTime"] - data.time))
                 {
                     Destroy(gameObject);
@@ -58,11 +37,10 @@ public class HoldNote : NoteBase
             }
 
         }
-        else if (hasStarted && !isHolding)
+        else
         {
             Destroy(gameObject);
             // hold released early, do any penalties before the destroy statement (same for success)
-            // doesnt really do much because there is no "let go of button" checking
         }
     }
 }
