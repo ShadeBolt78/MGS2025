dd8a871eebebbc4e182691a5d729527f055167dd
J-Talon <94146034+J-Talon@users.noreply.github.com>
2025-11-08 19:04:13 -0500

    Revert "Merge PlayerInput into Prototype (again. Attempt 2)"

---
+++ Assets/Scripts/testControllerbinds.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class testControllerbinds : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Yuhh");
+    }
+    void OnLane0()
+    {
+        Debug.Log("Lane 0 Pressed");
+    }
+    void OnLane1()
+            {
+         Debug.Log("Lane 1 Pressed");
+        }
+    void OnLane2()
+    {
+         Debug.Log("Lane 2 Pressed");
+
+        }
+    void OnLane3()
+            {
+         Debug.Log("Lane 3 Pressed");
+        }
+void OnLane4()
+    {
+         Debug.Log("Lane 4 Pressed");
+        }
+    
+}
