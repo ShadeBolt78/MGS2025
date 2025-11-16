8a941c0de1b61ce9c055fb2f38f264a42a5aec00
Angad Sidhu <94486808+AngadSidhu@users.noreply.github.com>
2025-11-16 17:40:45 -0500

    revamped how holdnotes function, edited csv to make hold note work, dead notes type shi

---
+++ Assets/Scripts/Notes/DeadNote.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DeadNote : NoteBase
+{
+    public override void OnKeyPressed()
+    {
+        AnimationManager.Missed(this.lane);
+        Health.TakeDamage(); // take damage when the note is hit
+        Destroy(gameObject);
+    }
+
+    public override void Miss()
+    {
+        Health.Regen(); // regen health or add score when missed
+        Destroy(gameObject);
+    }
+}
